b658e4956e96354bf41083bfc8056ab30ffa8c7d
Blaise1030 <blaisetiong00@gmail.com>
2021-11-04 13:30:24 +0800

    Added save changes button

--- src/Views/Creator.tsx
+++ src/Views/Creator.tsx
@@ -13,6 +13,7 @@ import ChevronLeftIcon from "@heroicons/react/solid/ChevronLeftIcon";
 import ChevronRightIcon from "@heroicons/react/solid/ChevronRightIcon";
 import getProjectData from "../api/getPageData";
 import LoadingIndicator from "../components/LoadingIndicator";
+import {putProjectData} from "../api/putPageData";
 
 const Creator = () => {
   const history = useHistory();
@@ -151,78 +152,111 @@ const ChangeBackgroundImage = () => {
     updateColumns,
     onAddComponent,
     backgroundImage,
+    packageSiteInfo,
     setBackgroundImage,
   } = useContext(CreatorContext);
   const match: {params: {id: string}} = useRouteMatch() as any;
+  const [loading, setLoading] = useState(false);
+
   const uploadFiles = (file: File) => {
     const url = URL.createObjectURL(file);
     setBackgroundImage(url);
   };
 
+  const updatePage = async () => {
+    setLoading(true);
+    await putProjectData(packageSiteInfo() as any, match.params.id);
+    setLoading(false);
+  };
+
   return (
-    <div className="fixed bottom-2 right-2 select-none w-300 z-20">
-      <div className="shadow-md border bg-white border-black rounded pl-5 pr-5 pb-5 pt-2">
-        <Disclosure>
-          {({open}) => (
-            <>
-              <Disclosure.Panel className="text-black">
-                <Dropzone
-                  onDrop={(acceptedFiles) => uploadFiles(acceptedFiles[0])}
-                >
-                  {({getRootProps, getInputProps}) => (
-                    <div className="m-auto">
-                      <div className="font-bold pb-5 mt-3">
-                        Background Image
-                      </div>
-                      <div className="flex flex-col w-full">
-                        <div className="m-auto w-20 h-20 border-2 border-dashed rounded flex flex-col items-center justify-center cursor-pointer object-cover">
-                          <div {...getRootProps()}>
-                            <input {...getInputProps()} />
-                            {!backgroundImage && (
-                              <PhotographIcon className="w-auto h-12 text-gray-300" />
-                            )}
-                            {backgroundImage && (
-                              <img
-                                src={backgroundImage}
-                                className="w-20 h-20 object-cover rounded"
-                              />
-                            )}
+    <div className="fixed bottom-8 right-8 select-none w-300 z-20">
+      <div className="shadow-md border bg-white border-black rounded relative">
+        {loading && <LoadingIndicator />}
+        <div className="p-4">
+          <Disclosure>
+            {({open}) => (
+              <>
+                <Disclosure.Panel className="text-black">
+                  <Dropzone
+                    onDrop={(acceptedFiles) => uploadFiles(acceptedFiles[0])}
+                  >
+                    {({getRootProps, getInputProps}) => (
+                      <div className="m-auto">
+                        <div className="font-bold pb-5 mt-3">
+                          Background Image
+                        </div>
+                        <div className="flex flex-col w-full">
+                          <div className="m-auto w-20 h-20 border-2 border-dashed rounded flex flex-col items-center justify-center cursor-pointer object-cover">
+                            <div {...getRootProps()}>
+                              <input {...getInputProps()} />
+                              {!backgroundImage && (
+                                <PhotographIcon className="w-auto h-12 text-gray-300" />
+                              )}
+                              {backgroundImage && (
+                                <img
+                                  src={backgroundImage}
+                                  className="w-20 h-20 object-cover rounded"
+                                />
+                              )}
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  )}
-                </Dropzone>
-                <div className="flex flex-row justify-between p-5 items-center">
-                  <ChevronLeftIcon
-                    className="h-8 w-8 cursor-pointer hover:shadow-lg rounded-full border border-black"
-                    onClick={() => updateColumns(-1)}
-                  />
-                  <div className="text-xl">{columns}</div>
-                  <ChevronRightIcon
-                    className="h-8 w-8 cursor-pointer hover:shadow-lg rounded-full border border-black"
-                    onClick={() => updateColumns(+1)}
-                  />
-                </div>
-                <div
-                  className="rounded cursor-pointer p-2 text-center border border-green-500 text-green-500 hover:bg-green-500 font-bold hover:text-white"
-                  onClick={onAddComponent}
-                >
-                  Add Tile
-                </div>
-              </Disclosure.Panel>
-              <Disclosure.Button className="w-full my-3 hover:bg-blue-200 bg-blue-100 p-2 rounded">
-                {open ? "Hide Settings" : "Settings"}
-              </Disclosure.Button>
-            </>
-          )}
-        </Disclosure>
-        <Link className="shadow-md" to={`/preview/${match.params.id}`}>
-          <div className="bg-blue-500 text-white font-semibold py-2 px-1 rounded w-full flex flex-row items-center justify-center">
-            <div>Next</div>
-            <ChevronRightIcon className="w-5 h-5 mt-0.5 shadow" />
+                    )}
+                  </Dropzone>
+                  <div className="flex flex-row justify-between p-5 items-center">
+                    <ChevronLeftIcon
+                      className="h-8 w-8 cursor-pointer hover:shadow-lg rounded-full border border-black"
+                      onClick={() => updateColumns(-1)}
+                    />
+                    <div className="text-xl">{columns}</div>
+                    <ChevronRightIcon
+                      className="h-8 w-8 cursor-pointer hover:shadow-lg rounded-full border border-black"
+                      onClick={() => updateColumns(+1)}
+                    />
+                  </div>
+                  <div
+                    className="rounded cursor-pointer p-2 text-center border border-green-500 text-green-500 hover:bg-green-500 font-bold hover:text-white"
+                    onClick={onAddComponent}
+                  >
+                    Add Tile
+                  </div>
+                </Disclosure.Panel>
+                <Disclosure.Button className="w-full my-3 hover:bg-blue-200 bg-blue-100 p-2 rounded">
+                  {open ? "Hide Settings" : "Settings"}
+                </Disclosure.Button>
+              </>
+            )}
+          </Disclosure>
+          <div className="w-full border "></div>
+          <div
+            onClick={updatePage}
+            className="
+              border 
+            border-blue-500  
+            text-blue-500               
+              font-semibold 
+              py-2 px-1 rounded 
+              w-full flex 
+              flex-row 
+              items-center 
+              justify-center 
+              my-2
+              duration-200
+              cursor-pointer
+              hover:shadow-md
+              "
+          >
+            <div>Save Changes</div>
           </div>
-        </Link>
+          <Link className="shadow-md" to={`/preview/${match.params.id}`}>
+            <div className="bg-blue-500 text-white font-semibold py-2 px-1 rounded w-full flex flex-row items-center justify-center">
+              <div>Next</div>
+              <ChevronRightIcon className="w-5 h-5 mt-0.5 shadow" />
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
